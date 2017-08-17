11ad37bcf78043a07a972c7c8a1af986bf559924
Jason Cheung <jayson.cheung@hotmail.com>
2017-08-17 13:15:17 -0700

    Styling for add to cart btn and image preview

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,12 +3,13 @@ chrome.extension.sendMessage({}, function(response) {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
 
+		var t0 = performance.now();
+
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");		
+		console.log("FFXIV - MogStation Shopping Cart Loading: scripts/inject.js");		
 		// ----------------------------------------------------------
 		
-		var t0 = performance.now();
 
 		// ----------------------------------------------------------		
 		// Objects 
@@ -19,8 +20,14 @@ chrome.extension.sendMessage({}, function(response) {
 			this.imgUrl = imgUrl;
 			this.sessionId = sessionId;	// used to determine if this product's ID is still valid. 
 		}
+
+		let PAGE_TYPE = Object.freeze({
+			HOME: "home",
+			DETAILS: "details", 
+		});
 		// ----------------------------------------------------------		
 			
+
 		// ----------------------------------------------------------		
 		// Helpers 
 		function mapToJson(map) {
@@ -44,11 +51,10 @@ chrome.extension.sendMessage({}, function(response) {
 			location.href = "javascript:contentEvent('select', 'skuidFOOBAR');"
 		});
 
-		var $newDiv2 = $("<div class='testButton'>Reset storage</div>");
+		var $newDiv2 = $("<div class='testButton'>Reset Storage</div>");
 		$newDiv2.click(function() {
-			console.log("Clicked on new div 2");
+			console.log("Clicked on Reset Storage button");
 			var ids = new Map();
-			// ids.set("foo", "bar");
 			console.log(ids);
 			var serialized = mapToJson(ids);
 			chrome.storage.sync.set({ "shoppingCart": serialized }, function(){
@@ -58,30 +64,30 @@ chrome.extension.sendMessage({}, function(response) {
 
 		var $newDiv3 = $("<div class='testButton'>Check storage</div>");
 		$newDiv3.click(function() {
-			console.log("Clicked on new div 3");        
-			chrome.storage.sync.get(/* String or Array */"shoppingCart", function(items){
-				//  items = [ { "yourBody": "myBody" } ]
+			console.log("Clicked on Check Storage button");        
+			chrome.storage.sync.get("shoppingCart", function(items){
 				console.log(items);
 				var products = jsonToMap(items.shoppingCart);
 				console.log(products);
 			});
 		});
 
+		var $updateCart = $("<div class='testButton'>Update Cart</div>");
+		$updateCart.click(function() {
+			console.log("Clicked on Update Cart button");        
+			updateCart();
+		});
+
 
 		// Shopping Cart Initialization 
-		var cachedCart = new Map();
+		var pageType = PAGE_TYPE.HOME; 
+		if ($("#shop_item_purchase_button").length) {
+			pageType = PAGE_TYPE.DETAILS;
+		}
+		console.log("Page type: " + pageType);
 
 		var currentSessionId = $("input[name='_st_acc']").attr('value');
 		console.log(currentSessionId);
-
-		var foobar = $("input[name='event']").attr('value');
-		console.log(foobar);
-
-		var $updateCart = $("<div class='testButton'>Update Cart</div>");
-		$updateCart.click(function() {
-			console.log("Clicked on update cart button");        
-			updateCart();
-		});
 
 		var $shoppingCart = $("<div id='shoppingCart'><h1>Shopping Cart</h1></div>");
 		var $itemCount = $("<span id='itemCount'>(0)</span>")
@@ -93,48 +99,71 @@ chrome.extension.sendMessage({}, function(response) {
 		$("body").append($buttonHolder);
 		// ----------------------------------------------------------
 
+		
 		// ----------------------------------------------------------
-		// Adding buttons into page 
+		// Adding buttons into page & Updating cart session ID's if possible. 
 		function createAddToCartButton(product) {
-			var btn = $("<button value=" + product.id + ">Add to Cart</button>");	// TODO: Product id here is useless. 
+			var btn = $("<input type='image' class='addToCartBtn' value=" + product.id + " src=" + chrome.extension.getURL('img/cart_icon.png') + ">");	// TODO: Product id here is useless. 
 			btn.click(function() {
 				var itemId = $(this).attr("value");
 				
 				// retrieve shopping cart
-				chrome.storage.sync.get("shoppingCart", function(items){
-					
-					console.log(items["shoppingCart"]);
-
+				chrome.storage.sync.get("shoppingCart", function(items) {					
 					// add or update cart
 					var shoppingCart = jsonToMap(items.shoppingCart);
 					shoppingCart.set(product.name, product);
 
 					// save changes to storage 
-					chrome.storage.sync.set({"shoppingCart": mapToJson(shoppingCart)}, function() {});					
+					chrome.storage.sync.set({"shoppingCart": mapToJson(shoppingCart)}, function() {});
+					
+					// update cart display
+					updateCart();
 				});
 			});
 			return btn;
 		}
 
-		// Adding buttons to items 
-		var $items = $(".content_box4"); // each item 
-		$items.each( function(index, element) {
-			var itemId = $(this).find("input").attr("value");
-			var title = $(this).find("td.text_left a").html();
-			var price = 0; 
-			if ($(this).find("div.price_bg").length) {  
-				// normal price 
-				price = $(this).find("div.price_bg div").html().replace("USD", "").trim();
-			} else {	
-				// discount price  
-				price = $(this).find("div.discount_bg span:last").html().replace("USD", "").trim();				
-			}
-			var imgUrl = $(this).find("div.img_space img:first").attr("src");
-			
-			var product = new Product(itemId, title, price, imgUrl, currentSessionId);
+		// Update cart session ID and adding buttons to items 
+		chrome.storage.sync.get("shoppingCart", function(items) {
+			var cachedCart = jsonToMap(items.shoppingCart);
 
-			var button = createAddToCartButton(product);
-			$(this).children(":first").append(button);
+			var updateCount = 0; // keep track of how many items were updated (debug)
+
+			var $items = $(".content_box4"); // each item 
+			$items.each( function(index, element) {
+				// retrieve product info from DOM
+				var itemId = $(this).find("input").attr("value");
+				var title = $(this).find("td.text_left a").html();
+				var price = 0; 
+				if ($(this).find("div.price_bg").length) {  
+					// normal price 
+					price = $(this).find("div.price_bg div").html().replace("USD", "").trim();
+				} else {	
+					// discount price  
+					price = $(this).find("div.discount_bg span:last").html().replace("USD", "").trim();				
+				}
+				var imgUrl = $(this).find("div.img_space img:first").attr("src");
+				
+				// create product object 
+				var product = new Product(itemId, title, price, imgUrl, currentSessionId);
+
+				// create and add button display 
+				var button = createAddToCartButton(product);
+				$(this).find("div.box_body .contents").children(":last").children(":first").append(button);	// append to price ribbon
+
+				// update item in cart 
+				var existingProduct = cachedCart.get(product.name);
+				if (existingProduct && existingProduct.currentSessionId != currentSessionId) {
+					cachedCart.set(product.name, product);
+					updateCount++;
+				}
+			});
+
+			if (updateCount > 0) {
+				chrome.storage.sync.set({"shoppingCart": mapToJson(cachedCart)}, function(){});
+				updateCart();
+				console.log("Updated " + updateCount + " items.");
+			}
 		});
 		// ----------------------------------------------------------
 				
@@ -150,7 +179,7 @@ chrome.extension.sendMessage({}, function(response) {
 				// keep track of total cost 
 				var total = 0;
 
-				// clear the list 
+				// clear the cart display 
 				$itemList.html('');
 
 				for (var [key, product] of shoppingCart) {
@@ -159,22 +188,23 @@ chrome.extension.sendMessage({}, function(response) {
 					var $item = $("<tr class='cartItem'></tr>");
 					var $name = $("<td><a>" + product.name + "</a></td>");
 					var $price = $("<td>" + product.price + "</td>");
-					var $buyBtn = $("<td><img class='cartIcon' src=" + chrome.extension.getURL('img/buy_icon.png') + "></td>");
+					// buy button removed as the backend uses _st_itemDetailData which is completely random.
+					// var $buyBtn = $("<td><img class='cartIcon' src=" + chrome.extension.getURL('img/buy_icon.png') + "></td>");
 					var $removeBtn = $("<td><img class='cartIcon' src=" + chrome.extension.getURL('img/remove_icon.png') + "></td>");
-					var $previewImg = $("<div class='hover_img'><img src=" + product.imgUrl + " height=100></div>")
+					var $previewImg = $("<div class='hover_img_container'><div class='hover_img_border'><img src=" + product.imgUrl + "></div></div>")
 
 					// attach event handlers 
 					if (product.sessionId === currentSessionId) {
 						$name.click({product: product}, onCartItemClick);	// pass product item (key 'product') to event handler.  
-						$buyBtn.click({product: product}, onCartItemClick);						
+						// $buyBtn.click({product: product}, onCartItemClick);						
 					} else {
 						$name.addClass("invalidSession");
-						$buyBtn.addClass("invalidSession");
+						// $buyBtn.addClass("invalidSession");
 					}
 					$removeBtn.click({product: product}, onCartItemRemoveClick);
 					
-					// add DOM elements to shopping cart
-					$item.append($name, $price, $buyBtn, $removeBtn, $previewImg);
+					// add to the cart display
+					$item.append($name, $price, $removeBtn, $previewImg);
 					$itemList.append($item);
 
 					// calculate the total 
@@ -188,7 +218,9 @@ chrome.extension.sendMessage({}, function(response) {
 		}
 
 		function onCartItemClick(event) {
-			$input = $('<input name="_pr_selectItem_string" type="radio" value=' + event.data.product.id + ' id="skuidFOOBAR" checked>');
+			$input = $('<input type="hidden" name=' 
+				+ (pageType == PAGE_TYPE.HOME ? "_pr_selectItem_string" : "_pr_selectRPItem_string") 
+				+ ' type="radio" value=' + event.data.product.id + ' id="skuidFOOBAR" checked>');
 			$("form").append($input);	
 			location.href = "javascript:contentEvent('select', 'skuidFOOBAR');";	// using function from original page. 
 		}
@@ -203,7 +235,8 @@ chrome.extension.sendMessage({}, function(response) {
 				if (shoppingCart.has(productName)) {
 					console.log("Removing " + productName + " from shopping cart");
 					shoppingCart.delete(productName);
-					chrome.storage.sync.set("shoppingCart", mapToJson(shoppingCart));
+					chrome.storage.sync.set({"shoppingCart": mapToJson(shoppingCart)}, function() {});
+					updateCart();
 				} else {
 					console.log("Warning: Could not remove item " + productName + " from cart - item not found");
 				}
@@ -214,8 +247,7 @@ chrome.extension.sendMessage({}, function(response) {
 		// ----------------------------------------------------------
 		
 		var t1 = performance.now();
-		console.log("Extension loaded. Took " + (t1 - t0) + " milliseconds.")
-
+		console.log("Extension loaded. Took " + (t1 - t0) + " milliseconds.");
 	}
 	}, 10);
 });
